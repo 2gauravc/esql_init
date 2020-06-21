5636743cde34b069dd041eaf4bcf68d16f723e63
Gaurav Chaturvedi <2gauravc@gmail.com>
2020-06-21 18:50:23 +0800

    init the repo

---
+++ table_creation.sql
@@ -0,0 +1,104 @@
+
+/*Table structure for table customers */
+DROP TABLE IF EXISTS customers CASCADE;
+CREATE TABLE customers (
+  customerNumber int NOT NULL,
+  customerName varchar(50) NOT NULL,
+  contactLastName varchar(50) NOT NULL,
+  contactFirstName varchar(50) NOT NULL,
+  phone varchar(50) NOT NULL,
+  addressLine1 varchar(50) NOT NULL,
+  addressLine2 varchar(50) DEFAULT NULL,
+  city varchar(50) NOT NULL,
+  state varchar(50) DEFAULT NULL,
+  postalCode varchar(15) DEFAULT NULL,
+  country varchar(50) NOT NULL,
+  salesRepEmployeeNumber int DEFAULT NULL,
+  creditLimit decimal(10,2) DEFAULT NULL,
+  PRIMARY KEY (customerNumber));
+
+/*Table structure for table employees */
+DROP TABLE IF EXISTS employees CASCADE;
+CREATE TABLE employees (
+  employeeNumber int NOT NULL,
+  lastName varchar(50) NOT NULL,
+  firstName varchar(50) NOT NULL,
+  extension varchar(10) NOT NULL,
+  email varchar(100) NOT NULL,
+  officeCode varchar(10) NOT NULL,
+  reportsTo int DEFAULT NULL,
+  jobTitle varchar(50) NOT NULL,
+  PRIMARY KEY (employeeNumber),
+  CONSTRAINT employees_ibfk_1 FOREIGN KEY (reportsTo) REFERENCES employees (employeeNumber));
+
+/*Table structure for table offices */
+DROP TABLE IF EXISTS offices CASCADE;
+CREATE TABLE offices (
+  officeCode varchar(10) NOT NULL,
+  city varchar(50) NOT NULL,
+  phone varchar(50) NOT NULL,
+  addressLine1 varchar(50) NOT NULL,
+  addressLine2 varchar(50) DEFAULT NULL,
+  state varchar(50) DEFAULT NULL,
+  country varchar(50) NOT NULL,
+  postalCode varchar(15) NOT NULL,
+  territory varchar(10) NOT NULL,
+  PRIMARY KEY (officeCode));
+
+/*Table structure for table orderdetails */
+DROP TABLE IF EXISTS orderdetails CASCADE;
+CREATE TABLE orderdetails(
+  orderNumber int NOT NULL,
+  productCode varchar(15) NOT NULL,
+  quantityOrdered int NOT NULL,
+  priceEach decimal(10,2) NOT NULL,
+  orderLineNumber smallint NOT NULL,
+  PRIMARY KEY (orderNumber,productCode)); 
+
+/*Table structure for table orders */
+DROP TABLE IF EXISTS orders CASCADE;
+CREATE TABLE orders (
+  orderNumber int NOT NULL,
+  orderDate date NOT NULL,
+  requiredDate date NOT NULL,
+  shippedDate date DEFAULT NULL,
+  status varchar(15) NOT NULL,
+  comments text,
+  customerNumber int NOT NULL,
+  PRIMARY KEY (orderNumber),
+  CONSTRAINT orders_ibfk_1 FOREIGN KEY (customerNumber) REFERENCES customers (customerNumber));
+
+/*Table structure for table payments */
+DROP TABLE IF EXISTS payments CASCADE;
+CREATE TABLE payments (
+  customerNumber int NOT NULL,
+  checkNumber varchar(50) NOT NULL,
+  paymentDate date NOT NULL,
+  amount decimal(10,2) NOT NULL,
+  PRIMARY KEY (customerNumber,checkNumber),
+  CONSTRAINT payments_ibfk_1 FOREIGN KEY (customerNumber) REFERENCES customers (customerNumber));
+
+/*Table structure for table productlines */
+DROP TABLE IF EXISTS productlines CASCADE;
+CREATE TABLE productlines (
+  productLine varchar(50) NOT NULL,
+  textDescription varchar(4000) DEFAULT NULL,
+  htmlDescription text,
+  image BYTEA,
+  PRIMARY KEY (productLine));
+
+
+/*Table structure for table products */
+DROP TABLE IF EXISTS products CASCADE;
+CREATE TABLE products (
+  productCode varchar(15) NOT NULL,
+  productName varchar(70) NOT NULL,
+  productLine varchar(50) NOT NULL,
+  productScale varchar(10) NOT NULL,
+  productVendor varchar(50) NOT NULL,
+  productDescription text NOT NULL,
+  quantityInStock smallint NOT NULL,
+  buyPrice decimal(10,2) NOT NULL,
+  MSRP decimal(10,2) NOT NULL,
+  PRIMARY KEY (productCode),
+  CONSTRAINT products_ibfk_1 FOREIGN KEY (productLine) REFERENCES productlines (productLine));
